b30a055028d9c8dc442f86ab6ce52d71ef0f526a
glebdulesov-alt <gleb.dulesov@gmail.com>
2026-04-09 09:18:11 +0200

    fix ports-head

--- snake/snake.srcs/sources_1/new/head.vhd
+++ snake/snake.srcs/sources_1/new/head.vhd
@@ -4,6 +4,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity head is
     Port ( clk : in STD_LOGIC;
            rst : in STD_LOGIC;
+           en_speed :in STD_LOGIC;
+           bite_itself : in STD_LOGIC;
            btn_press : in STD_LOGIC;
            btn_data : in STD_LOGIC_VECTOR (3 downto 0);
            x_pos : out STD_LOGIC_VECTOR (2 downto 0);

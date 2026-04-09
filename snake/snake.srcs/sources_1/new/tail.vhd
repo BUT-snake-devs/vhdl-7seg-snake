4fa7c2e6dc21bc3eab3ad45a0fc452718acafd93
daniil-yeriemieiev <272711@vut.cz>
2026-04-09 08:44:33 +0200

    Create Project

---
+++ snake/snake.srcs/sources_1/new/tail.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+---------------------------------------------------------
+entity tail is
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           x_pos_i : in STD_LOGIC_VECTOR (2 downto 0);
+           y_pos_i : in STD_LOGIC_VECTOR (1 downto 0);
+           lenght : in STD_LOGIC_VECTOR (5 downto 0);
+           x_pos_o : out STD_LOGIC_VECTOR (2 downto 0);
+           y_pos_o : out STD_LOGIC_VECTOR (1 downto 0));
+end tail;
+---------------------------------------------------------
+architecture Behavioral of tail is
+
+begin
+
+
+end Behavioral;

80d10a5af6a0dc84b0ad1414d54f41ca036de121
artembal27104-beep <artembal.27104@gmail.com>
2026-04-15 21:02:20 +0200

    Update btn_ctrl.vhd

--- snake/snake.srcs/sources_1/new/btn_ctrl.vhd
+++ snake/snake.srcs/sources_1/new/btn_ctrl.vhd
@@ -13,8 +13,32 @@ entity btn_ctrl is
 end btn_ctrl;
 ---------------------------------------------------------
 architecture Behavioral of btn_ctrl is
-
+    signal btn_state_prev : std_logic_vector(3 downto 0) := "0000";
+    signal btn_state_curr : std_logic_vector(3 downto 0) := "0000";
+    signal btn_diff       : std_logic_vector(3 downto 0);
 begin
+    btn_diff <= btn_state_curr and not btn_state_prev;
 
-
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                btn_state_prev <= "0000";
+                btn_state_curr <= "0000";
+                btn_press      <= '0';
+                btn_data       <= "00";
+            else
+                btn_state_curr <= btnu & btnr & btnd & btnl;
+                btn_state_prev <= btn_state_curr;
+                btn_press <= '0';
+                case btn_diff is
+                    when "1000" => btn_press <= '1'; btn_data <= "00"; -- UP
+                    when "0100" => btn_press <= '1'; btn_data <= "01"; -- RIGHT
+                    when "0010" => btn_press <= '1'; btn_data <= "10"; -- DOWN
+                    when "0001" => btn_press <= '1'; btn_data <= "11"; -- LEFT
+                    when others => null; 
+                end case;
+            end if;
+        end if;
+    end process;
 end Behavioral;
